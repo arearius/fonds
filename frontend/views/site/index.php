95f15486b66bfdb02a04f02b20b0a891b32429bc
Rearius <arearius@gmail.com>
2018-09-14 20:00:21 +0300

    Изменение страницы приветствия

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -11,7 +11,7 @@ $this->title = 'Фонды';
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii. New Project Fonds</a></p>
     </div>
 
     <div class="body-content">
